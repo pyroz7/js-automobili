76b38bd99680acb112b26db204c28e06f51d12cb
pyroz7 <vincipyroz7@gmail.com>
2023-11-22 16:14:39 +0100

    Half

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//CREARE UN ARREY COMPOSTO DA  10 AUTOMOBILI,OGNI AUTOMOBILE AVRà PROPRIETà DIVERSE.
+
+
+let automobili = [
+    { marca: "Ferrari", modello: 'Spider', alimentazione: 'Benzina' },
+    { marca: 'Ford', modello: 'Focus', alimentazione: 'Benzina' },
+    { marca: 'Maserati', modello: 'Cabrio', alimentazione: 'Benzina' },
+    { marca: 'Nissan', modello: 'Qasqai', alimentazione: 'Diesel' },
+    { marca: 'Golf', modello: 'Serie 7', alimentazione: 'Diesel' },
+    { marca: 'Lamborghini ', modello: 'Aventador', alimentazione: 'Disel' },
+    { marca: 'Renault ', modello: 'Clio', alimentazione: 'Gasolio' },
+    { marca: 'Subaru ', modello: 'Sport', alimentazione: 'Elettrica' },
+    { marca: 'Audi', modello: 'A7', alimentazione: 'Gasolio' },
+    { marca: 'Fiat', modello: 'Spider', alimentazione: 'Gasolio' }
+];
